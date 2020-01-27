cd06e0617cec94dee325408522659251b8737bc5
Eli Cornblath <eli.cornblath@uphs.upenn.edu>
2020-01-27 11:45:00 -0500

    analyze exploratory factory analysis loadings, scores, and prediction of CSF

--- code/kmeans/AnalyzeEFA.R
+++ code/kmeans/AnalyzeEFA.R
@@ -18,16 +18,21 @@ INDDIDs <- read.csv(paste(params$opdir,'processed/microSample.csv',sep=''),strin
 ### factor analysis ###
 #######################
 
+# load imputed data, name features based on short names with _ in between region and type
 microSample.imp <- complete(microSample.imp,1)
+microSample.imp <- micro.order.by(as.matrix(microSample.imp))
+colnames(microSample.imp) <- colnames(micro.order.by(as.matrix(microSample),vers='short'))
+
 load(file = paste0(params$opdir,'micekmeans/FactorAnalysis.RData'))
+rownames(loadings) <- colnames(microSample.imp) # name loading features
 
 featureTypes <- list(Syn='Syn',Tau='Tau',TDP43='TDP43',Thio='Thio',
-                     NeocorticalAtrophy=c('MFNeuronLoss','MFGliosis'),
-                     LimbicAtrophy=c('CSNeuronLoss','CSGliosis'),
-                     BasalGangliaAtrophy=c('CPNeuronLoss','CPGliosis'),
-                     MedullaAtrophy=c('MedNeuronLoss','MedGliosis'),
-                     CerebellumAtrophy=c('CBNeuronLoss','CBGliosis'),
-                     MidbrainAtrophy=c('MBNeuronLoss','MBGliosis'),
+                     NeocorticalAtrophy=c('MF_NeuronLoss','MF_Gliosis'),
+                     LimbicAtrophy=c('CS_NeuronLoss','CS_Gliosis'),
+                     BasalGangliaAtrophy=c('CP_NeuronLoss','CP_Gliosis'),
+                     MedullaAtrophy=c('Med_NeuronLoss','Med_Gliosis'),
+                     CerebellumAtrophy=c('CB_NeuronLoss','CB_Gliosis'),
+                     MidbrainAtrophy=c('MB_NeuronLoss','MB_Gliosis'),
                      Angiopathy='Angiopathy') # find factors that have highest mean weights on each type of feature
 #featureTypes.name <- lapply(featureTypes,function(X) paste(X,collapse = ''))
 featureTypes.name <- names(featureTypes)
@@ -41,9 +46,15 @@ if(length(unique(featureMeans.max)) == length(featureMeans.max)){
   colnames(loadings)[featureMeans.max] <- names(featureMeans.max)
 }
 
+p <- imagesc(t(loadings),cmap='Blues') + 
+  theme(axis.text.x = element_text(angle=90,hjust=1,vjust=0.5),text=element_text(size=6)) +
+  theme(legend.key.size = unit(0.15,'cm'))
+ggsave(filename = paste0(savedir,'FactorLoadings.pdf'),plot = p,
+       height = 6,width=18,units='cm')
+
 p <- imagesc(cor(scores),cmap='redblue',clim=c(-1,1)) + coord_equal() +
   theme(axis.text.x  = element_text(angle=90,hjust=1,vjust=0.5),text=element_text(size=6)) +
-  theme(legend.key.size = unit(0.1,'cm'))
+  theme(legend.key.size = unit(0.15,'cm'))
 ggsave(filename = paste0(savedir,'FactorCorrelation.pdf'),plot = p,
        height = 6,width=6,units='cm')
 
@@ -74,11 +85,13 @@ m.list <- lapply(df.reg, function(df) summary(lm(y~.,data=df)))
 names(m.list) <- featureTypes.name
 
 r <- sapply(m.list, function(X) X$r.squared)
-p <- ggplot() + geom_col(aes(x=names(r),y=r)) + theme_pubr()+
-  theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5),text=element_text(size=8)) +
+p <- ggplot() + geom_col(aes(x=names(r),y=r),fill='light blue') + theme_pubr()+
+  scale_y_continuous(limits=c(0,0.5))+ ggtitle('Variance Explained by CSF') +
+  theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5),text=element_text(size=6),
+        plot.title=element_text(size=8,hjust=0.5)) +
   ylab('R^2') + xlab('')
 ggsave(filename = paste0(savedir,'FactorsCSFRegress.pdf'),plot = p,
-       height = 6,width=6,units='cm')
+       height = 5,width=6,units='cm')
 
 # repeat but don't regress
 df.reg <- lapply(featureTypes.name, function(n) cbind(data.frame(y=df.CSF[,n]),df.CSF[,CSF.vars]))
@@ -86,9 +99,24 @@ m.list <- lapply(df.reg, function(df) summary(lm(y~.,data=df)))
 names(m.list) <- featureTypes.name
 
 r <- sapply(m.list, function(X) X$r.squared)
-p <- ggplot() + geom_col(aes(x=names(r),y=r)) + theme_pubr()+
-  theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5),text=element_text(size=8)) +
+pval <- sapply(m.list, function(X) pf(X$fstatistic['value'],X$fstatistic['numdf'],X$fstatistic['dendf'],lower.tail = F))
+pval <- p.signif(p.adjust(pval,method='fdr'))
+p <- ggplot() + geom_col(aes(x=names(r),y=r),fill='light blue') + theme_pubr()+
+  annotate(geom='text',x=names(r),y=0.4,label=pval,size=2.5) + 
+  scale_y_continuous(limits=c(0,0.5))+ ggtitle('Variance Explained by CSF') +
+  theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5),text=element_text(size=6),
+        plot.title=element_text(size=8,hjust=0.5)) +
   ylab('R^2') + xlab('')
 ggsave(filename = paste0(savedir,'FactorsCSF.pdf'),plot = p,
-       height = 6,width=6,units='cm')
+       height = 5,width=6,units='cm')
 
+p <- ggplot() + geom_line(aes(x=1:length(featureTypes.name),y=unname(cumsum(explained))),color='red',alpha=0.5)+ 
+  geom_line(aes(x=1:length(featureTypes.name),y=unname(explained)),color='light blue')+  theme_pubr()+
+  scale_x_continuous(limits = c(1,length(featureTypes.name)),breaks=1:length(featureTypes.name),labels=featureTypes.name)+
+  scale_y_continuous(limits=c(0,1)) + ggtitle('Variance Explained in Pathology') + 
+  theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5),text=element_text(size=6),
+        plot.title=element_text(size=8,hjust=0.5)) +
+ ylab('R^2') + xlab('')
+p
+ggsave(filename = paste0(savedir,'FactorsVarianceExplained.pdf'),plot = p,
+       height = 5,width=6,units='cm')
